ad9bd04e6495bf8900385ede0a3bda19364eb39d
efranor <efraoviedo@gmail.com>
2022-07-22 11:59:28 -0500

    se reescribio el .js ,con modificaciones

--- js/Tijeras.js
+++ js/Tijeras.js
@@ -1,78 +1,83 @@
+const EMPATE = 0;
+const GANASTE = 1;
+const PERDISTE = 2;
 
-function computerPlay(minimo, maximo){
-      let numero = Math.floor( Math.random() * (maximo-minimo +1)+ minimo  );
-      return numero;
+const Rock = 'rock';
+const Paper = 'paper';
+const Scissors = 'scissors';
+
+
+const rockBtn = document.getElementById('rock');
+const paperBtn = document.getElementById('paper');
+const scissorsBtn = document.getElementById('scissors');
+const startGame = document.getElementById('start-game');
+const userImg = document.getElementById('user-img');
+const machineImg = document.getElementById('machine-img');
+
+rockBtn.addEventListener('click', () => {
+      show(Rock);
+});
+
+paperBtn.addEventListener('click', () => {
+     show(Paper);
+
+});
+scissorsBtn.addEventListener('click', () => {
+      show(Scissors);
+
+});
+
+function show (playerSelection) {
+      const computerSelection = calcComputerSelection();
+      const result = calcResult(playerSelection,computerSelection);
+      userImg.src = 'img '+playerSelection+'.png';     
+      machineImg.src = 'img '+computerSelection+'.png';
+
+      switch(result){
+            case EMPATE:
+                  startGame.innerHTML = 'ES UN EMPATE !';
+                  break;
+            case GANASTE:
+                  startGame.innerHTML = 'GANASTE !';
+            break;
+            case PERDISTE:
+                  startGame.innerHTML = 'PERDISTE !';
+            break;
+      }
 }
 
-const rock = 0;
-const paper = 1;
-const scissors = 2;
-
-// let opciones = ["Piedra","Papel","Tijeras"];
-
-// alert( opciones[1] );
-
-let playerSelection;
-let computerSelection = computerPlay(0,2);
-
-playerSelection = prompt("¿que elijes\nrock: 0\nPaper: 1\nscissors:2",0);
-
-
-if(playerSelection == rock){
-      alert("elejiste piedra!");
-      if(computerSelection == rock)
-      alert("You tied");
-      else if(computerSelection == paper){
-            alert("you lost");
-      }else if(computerSelection ==  scissors){
-            alert("you won");
+function calcComputerSelection() {
+      const number = Math.floor(Math.random() * 3);
+      switch (number) {
+            case 0:
+                  return Rock;
+                  case 1:
+                        return Paper;
+                        case 2:
+                              return Scissors;
       }
-           
-      
-}else if(playerSelection == paper){
-      alert("elejiste papel!");
-      if(computerSelection == rock)
-      
-      alert("you won");
-      else if(computerSelection== paper){
-            alert("You tied");
-      }else if(computerSelection ==  scissors){
-            alert("you lost");
-      }
-      
-}else if(playerSelection == scissors){
-      alert("elejiste tijeras!");
-
-      if(computerSelection == rock)
-      alert("you lost");
-      else if(computerSelection == paper){
-            alert("you won");
-      }else if(computerSelection ==  scissors){
-            alert("You tied");
-      }
-}else {
-      alert("no jugaste,porque?");
 }
 
+function calcResult(playerSelection,computerSelection) {   
+    if(playerSelection === computerSelection){
+      return EMPATE;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    }else if(playerSelection === Rock){
+       if(computerSelection === Paper)
+       return PERDISTE;
+       if(computerSelection === Scissors)
+       return GANASTE;           
+      
+    }else if(playerSelection === Paper){ 
+          if(computerSelection === Scissors)
+          return PERDISTE;
+          if(computerSelection ===  Rock)
+          return GANASTE;         
+          
+    }else if(playerSelection === Scissors){ 
+          if(computerSelection === Rock)
+          return PERDISTE;
+          if(computerSelection === Paper)
+          return GANASTE;
+    }
+}
